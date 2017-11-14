8199868c33bb29b0eac65e6269125751e54d31b2
nico1988 <410827024@qq.com>
2017-11-14 14:23:49 +0800

    prototype

--- four/prorotype.js
+++ four/prorotype.js
@@ -2,3 +2,8 @@ var object = {};
 var prototype = Object.getPrototypeOf(object);
 console.log(Object.prototype == prototype);
 console.log(Object.prototype.isPrototypeOf(object));
+console.log(object.toString());
+object.toString = "abc";
+console.log(object.toString);
+delete object.toString;
+console.log(object.toString);
